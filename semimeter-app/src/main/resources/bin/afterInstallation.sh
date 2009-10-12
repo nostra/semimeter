94d589b8c1549dd215893ade2df38613aaceb8c4
Erlend Nossum <nossum@gmail.com>
2009-10-12 10:39:19 +0000

    Added separate app for semimeter.

---
+++ semimeter-app/src/main/resources/bin/afterInstallation.sh
@@ -0,0 +1,61 @@
+#!/bin/bash
+
+# Perform tasks which are not performed (easily) by the
+# app assembler
+
+executeDirectory=`dirname $0`
+# Need to change to the correct directory in order to 
+# get relative paths to be correct
+echo "Changing directory to: " ${executeDirectory}
+cd ${executeDirectory}
+
+chmod a+x *.sh
+
+# Rename jetty jars so that they do not have version number.
+# The sed expression is not very good, but as long as it works...
+cd ../lib
+for a in `ls | grep "jetty\|start"` 
+do
+  moveTo=`echo $a | sed s/-[0-9]*\.[0-9]*\.[0-9]*\.jar/.jar/g`
+  if [ "$moveTo" != "$a" ]
+  then
+    echo "Moving " $a" to " $moveTo 
+    mv -i $a $moveTo
+  fi 
+done
+
+# Rename also the war file(s)
+for a in `ls | grep "\.war"` 
+do
+  moveTo=`echo $a | sed s/-[0-9]*\.[0-9]*\.[0-9]*[-SNAPSHOT]*\.war/.war/g`
+  if [ "$moveTo" != "$a" ]
+  then
+    # Move the war file(s), it if exists in the wrong directory.
+    echo "Moving " $a" to ../webapps/"$moveTo 
+    mv -i $a ../webapps/$moveTo
+  fi 
+done
+
+# Back to the bin directory:
+cd ../bin
+
+# Remove place holder files
+rm -f ../logs/readme.txt
+rm -f ../webapps/readme.txt
+rm -f ../tmp/readme.txt
+rm -f ../lib/semimeter-app*
+
+echo
+echo "Notice that the application needs a valid configuration structure"
+echo "in /etc/api"
+sleep 1
+echo
+echo "Go to the bin directory."
+echo "You start the application by writing (in the bin directory)"
+echo "export JETTY_HOME=`pwd`/.."
+echo "export TMP=\$JETTY_HOME/tmp"
+echo "export JETTY_RUN=\$TMP"
+echo "export JETTY_LOG=\$JETTY_HOME/logs"
+echo "echo mkdir -p \$TMP \$JETTY_LOG"
+echo 'export JAVA_OPTIONS="-Djava.awt.headless=true -Xmx256m -Xms64m"'
+echo "./jetty.sh start # In order to background the process."
