29f8b6c0e1b3ac63bcee96f9c8208a4aa6a0795b
Sven Eigenbrodt <sven@hakkovb.(none)>
2011-03-21 22:05:02 +0100

    started changing persistance layer to mongodbnothing works yet. there is only some dependencies changed, and the dao is totally useless atm

---
+++ semimeter-webapp/src/main/java/org/semispace/semimeter/dao/MongoDbFactory.java
@@ -0,0 +1,40 @@
+package org.semispace.semimeter.dao;
+
+import com.mongodb.DB;
+import com.mongodb.Mongo;
+import org.springframework.beans.factory.FactoryBean;
+import org.springframework.beans.factory.annotation.Required;
+import org.springframework.util.Assert;
+
+public class MongoDbFactory implements FactoryBean<DB> {
+
+       private Mongo mongo;
+       private String name;
+
+       @Override
+       public DB getObject() throws Exception {
+           Assert.notNull(mongo);
+           Assert.notNull(name);
+           return mongo.getDB(name);
+       }
+
+      @Override
+      public Class<?> getObjectType() {
+          return DB.class;
+      }
+
+      @Override
+      public boolean isSingleton() {
+          return true;
+      }
+
+      @Required
+      public void setMongo(Mongo mongo) {
+          this.mongo = mongo;
+      }
+
+      @Required
+      public void setName(String name) {
+          this.name = name;
+      }
+}
